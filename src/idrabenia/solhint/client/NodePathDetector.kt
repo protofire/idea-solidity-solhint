b86fa2b5557baa051e09eeca38a84bbb4f4224a2
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-11-17 13:06:56 +0300

    Implement advanced algorithm for find Node installations on user's machine #9

--- src/idrabenia/solhint/client/NodePathDetector.kt
+++ src/idrabenia/solhint/client/NodePathDetector.kt
@@ -2,6 +2,7 @@ package idrabenia.solhint.client
 
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil.findAllExeFilesInPath
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil.findInPath
+import java.lang.System.getProperty
 
 
 object NodePathDetector {
@@ -10,13 +11,20 @@ object NodePathDetector {
          nodeWithSolhint() ?: findInPath("node")?.absolutePath ?: "node"
 
     fun detectAllNodePaths() =
-        findAllExeFilesInPath("node").map { it.absolutePath }
+        findAllExeFilesInPath(nodeBinName()).map { it.absolutePath }
 
     fun nodeWithSolhint() =
-        findAllExeFilesInPath("node")
+        findAllExeFilesInPath(nodeBinName())
             .filter { it.resolveSibling("solhint").exists() }
             .firstOrNull()
             ?.absolutePath
+
+    fun nodeBinName() =
+        if (getProperty("os.name").contains("windows", true)) {
+            "node.exe"
+        } else {
+            "node"
+        }
 
 }
 
